0dcdf41dc8f2fc6d696e63be0aac166dcfffcb3f
RazaK189 <rk180901@gmail.com>
2021-11-12 19:58:13 +0000

    used throw new array exception, had to set arguments == 0

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,7 +8,6 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-try {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -16,7 +15,10 @@ try {
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-           
+     try {
+        if (args.lenght == 0 ) {
+            throw new ArrayIndexOutOfBoundsException();
+        }      
         while(num != 0)
         {
             rem=num%16;
