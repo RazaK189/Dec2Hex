e011bab3725cce824469b62338851fe8b0c6ea9a
RazaK189 <rk180901@gmail.com>
2021-11-12 19:34:32 +0000

    moved try

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -8,6 +8,7 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+try {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
@@ -15,7 +16,7 @@ class Dec2Hex
         String hexadecimal="";
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
-            try {
+           
         while(num != 0)
         {
             rem=num%16;
